c44de6365cddb67db2aeb85296ebddf821702a28
Lunam2027 <milesluna744@gmail.com>
2026-02-11 13:00:04 -0700

    Create installer.lua

---
+++ installer.lua
@@ -0,0 +1,2 @@
+shell.run("wget https://raw.githubusercontent.com/lyrae-zip/CC-Tweaked-spreadsheet-via-AE2/refs/heads/main/items.lua")
+shell.run("wget https://raw.githubusercontent.com/lyrae-zip/CC-Tweaked-spreadsheet-via-AE2/refs/heads/main/config.txt")
